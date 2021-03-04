0bd7627dfc9efcc0446f592d282639aa936a914e
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-04 13:55:52 -0500

    json issue solved

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const products = require('./routes/product');
 module.exports = app;
