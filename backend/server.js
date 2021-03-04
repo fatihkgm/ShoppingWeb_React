e62bb89f6023a52727a971354e0b16a7a0188dd7
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-04 16:35:13 -0500

    mongoose connection done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const app = require('./app');
+const connectDatabase = require('./config/database');
 
 const dotenv = require('dotenv');
 
 //setting up config file
 dotenv.config({ path: 'backend/config/config.env' });
+
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port : ${process.env.PORT} `);
